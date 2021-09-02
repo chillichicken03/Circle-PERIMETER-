bae8f2f7ca78f7196b19895d5f956a1d54db85cf
apersontho <88486285+chillichicken03@users.noreply.github.com>
2021-09-02 06:26:31 +0000

    Initial commit

---
+++ How to use.md
@@ -0,0 +1,14 @@
+a > (3.1415926535)
+
+b > (3.14)
+
+c > (22/7) 
+
+Use a,b or c. Make sure its not cap
+
+diameter = (10) --> change to your diameter or if its in radius, times 2.
+
+pi = b --> change to a, b or c. 
+
+the rest DO NOT TOUCH.
+
---
+++ main.py
@@ -0,0 +1,26 @@
+a = (3.1415926535)
+b = (3.14)
+c = (22/7)
+diameter = (10)
+pi = b
+
+
+circumfence = diameter*pi
+roundedcir = round(circumfence,4)
+print(roundedcir, "circumfence")
+
+semicircle = (roundedcir * 1/2) + diameter
+roundedsemi = round(semicircle,4)
+print(roundedsemi, "semicircle")
+
+quartant = (roundedcir * 1/4) + diameter
+roundedqua = round(quartant,4)
+print(roundedqua, "quartant")
+
+print("end")
+
+
+
+
+
+
